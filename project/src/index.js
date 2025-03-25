96996b83ec6acea2c97a2c36c2eb78ec7deddbed
Aayush Gauutam <professional.atus6693@gmail.com>
2025-03-25 22:18:37 +0545

    add cors and dotenv packages; set up express middleware and database connection

--- project/src/index.js
+++ project/src/index.js
@@ -1,1 +1,11 @@
-console.log('hello from server: aayush');
+import dotenv from 'dotenv';
+import { app } from './app.js';
+
+dotenv.config({
+  path: './.env',
+});
+
+const port = process.env.PORT || 3000;
+app.listen(() => {
+  console.log(`server is listening at ${port}`);
+});
